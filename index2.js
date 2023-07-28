edc98bc66784f1a612ec1e80d079d5dbdd9aef09
Canezin15 <jp1603207@gmail.com>
2023-07-28 09:27:59 -0400

    commit 2°

--- index2.js
+++ index2.js
@@ -1,4 +1,5 @@
-class alunos{
+//class alunos
+class Alunos{
     constructor(matricula, nome, prova1, prova2, trabalho){
         this.matricula = matricula;
         this.nome = nome;
@@ -6,4 +7,28 @@ class alunos{
         this.prova2 = prova2;
         this.trabalho = trabalho;
     }
+
+//imprime dados
+impreme_dados(){
+    return this.matricula + " " + this.nome + " " + this.prova1 + " " + this.prova2 + " " + this.trabalho
 }
+
+//calculo de notas
+media(){
+    let media_P = (this.prova1 + this.prova2) * 0.25
+    let media_T = this.trabalho * 0.2
+    let media_F =  media_P + media_T
+
+    if(media_F > 6){
+        return 0 + " pontos"
+    }else{
+        return 6 - media_F.toFixed() + " pontos"
+    }
+
+}
+
+}
+
+const alunos = new Alunos("IF", "Canezin", 5, 5, 5)
+console.log(alunos.media())
+
